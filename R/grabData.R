a3ba0ae89b592e97dfab746d02cc4161429b0752
wkumler <wkumler@uw.edu>
2020-11-10 23:09:15 -0800

    Refactored mzML code and split into mzML vs mzXML files

--- R/grabData.R
+++ R/grabData.R
@@ -36,39 +36,6 @@ grabMzmlBPC <- function(filename, TIC=FALSE){
 
 
 
-#' Grab the BPC or TIC for a given mzXML file
-#'
-#' @param filename The name of the mzXML file to be read.
-#' @param TIC Should the BPC or TIC be read? If TIC=TRUE,
-#' the total ion current (TIC)
-#' for each retention time is read in. Otherwise, the BPC
-#' (base peak chromatogram) is read for each retention time, corresponding
-#' to the *maximum* intensity for each scan.
-#'
-#' @return A data.frame object with columns for retention time (rt) in minutes and
-#' and intensity (int) corresponding to the TIC or BPC for a given file.
-#'
-#' @export
-#'
-#' @examples
-#' mzXML_filename <- system.file("extdata", "190715_Poo_TruePooFK180310_Full2.mzXML", package = "RaMS")
-#' grabMzxmlBPC(mzXML_filename)
-#' grabMzxmlBPC(mzXML_filename, TIC=TRUE)
-grabMzxmlBPC <- function(filename, TIC=FALSE){
-  mz_xml <- xml2::read_xml(filename)
-
-  scan_nodes <- xml2::xml_find_all(mz_xml, '//d1:scan')
-  rt_chrs <- xml2::xml_attr(scan_nodes, "retentionTime")
-  rt_vals <- as.numeric(gsub(pattern = "PT|S", replacement = "", rt_chrs))/60
-
-  int_attr <- ifelse(TIC, "totIonCurrent", "basePeakIntensity")
-  int_vals <- as.numeric(xml2::xml_attr(scan_nodes, int_attr))
-
-  return(data.frame(rt=rt_vals, int=int_vals))
-}
-
-
-
 #' Read an mzML file into a data.frame
 #'
 #' @details This function reads an mzML file into R's working memory. mzML files
@@ -90,38 +57,16 @@ grabMzxmlBPC <- function(filename, TIC=FALSE){
 grabMzmlData <- function(filename){
   xml_data <- xml2::read_xml(filename)
 
-  rt_xpath <- '//d1:cvParam[@name="scan start time"]'
-  rt_nodes <- xml2::xml_find_all(xml_data, rt_xpath)
-  rt_vals <- as.numeric(xml2::xml_attr(rt_nodes, "value"))
+  file_metadata <- grabMzmlMetadata(xml_data)
+  ms1_nodes <- xml2::xml_find_all(
+    xml_data, '//d1:cvParam[@name="MS1 spectrum"]/parent::d1:spectrum'
+  )
 
-  #SHOULD PROB BE COMBINED INTO SINGLE "GRAB FILE METADATA" FUNCTION
-  compr <- getCompression(xml_data)
-  mz_precision <- getMzPrecision(xml_data)
-  int_precision <- getIntPrecision(xml_data)
+  rt_vals <- grabSpectraRt(ms1_nodes)
 
-  mz_xpath <- paste0('//d1:spectrum/d1:binaryDataArrayList',
-                     '/d1:binaryDataArray[1]/d1:binary')
-  mz_vals <- xml2::xml_text(xml2::xml_find_all(xml_data, mz_xpath))
-  mz_vals <- lapply(mz_vals, function(binary){
-      decoded_binary <- base64enc::base64decode(binary)
-      raw_binary <- as.raw(decoded_binary)
-      decomp_binary <- memDecompress(raw_binary, type = compr)
-      final_binary <- readBin(decomp_binary, what = "double",
-                              n=length(decomp_binary)/mz_precision,
-                              size = mz_precision)
-    })
+  mz_vals <- grabSpectraMz(ms1_nodes, file_metadata)
 
-  int_xpath <- paste0('//d1:spectrum/d1:binaryDataArrayList',
-                      '/d1:binaryDataArray[2]/d1:binary')
-  int_vals <- xml2::xml_text(xml2::xml_find_all(xml_data, int_xpath))
-  int_vals <- lapply(int_vals, function(binary){
-    decoded_binary <- base64decode(binary)
-    raw_binary <- as.raw(decoded_binary)
-    decomp_binary <- memDecompress(raw_binary, type = compr)
-    final_binary <- readBin(decomp_binary, what = "double",
-                            n=length(decomp_binary)/int_precision,
-                            size = int_precision)
-  })
+  int_vals <- grabSpectraInt(ms1_nodes, file_metadata)
 
   data.frame(rt=rep(rt_vals, sapply(mz_vals, length)),
              mz=unlist(mz_vals), int=unlist(int_vals))
@@ -129,36 +74,45 @@ grabMzmlData <- function(filename){
 
 
 
-#' Read an mzXML file into a data.frame
+
+#' Helper function to extract mzML file metadata
 #'
-#' @details This function reads an mzXML file into R's working memory. mzXML files
-#' are fundamentally XML documents, which allows rapid access to the data by
-#' parsing the XML. The R package `xml2::` is used for this purpose here.
-#' Retention time information can be read directly, while *m/z* and intensity
-#' information must be decoded from binary. To read data from the mzML format
-#' instead, check out grabMzmlData.
+#' @param xml_data mzML data parsed by xml2
 #'
-#' @param filename The name of the mzXML file to be read.
-#'
-#' @return A data.frame object with columns for retention time (rt) in minutes,
-#' m/z (mz), and intensity (int).
+#' @return A list of values used by other parsing functions, such as
+#' compression, mz_precision, int_precision
 #'
 #' @export
+grabMzmlMetadata <- function(xml_data){
+  compr_xpath <- '//d1:cvParam[@accession="MS:1000574"]'
+  compr_type <- xml2::xml_attr(xml2::xml_find_first(xml_data, compr_xpath), "name")
+  compr <- switch(compr_type,
+                  `zlib compression`="gzip",
+                  `none`="none")
+
+  mz_precision_xpath <- '//d1:cvParam[@accession="MS:1000523"]'
+  mz_bit_type <- xml_attr(xml_find_first(xml_data, mz_precision_xpath), "name")
+  mz_precision <- sub(mz_bit_type, pattern = "-bit float", replacement = "")
+  mz_precision <- as.numeric(mz_precision)/8
+
+  int_bit_xpath <- '//d1:cvParam[@accession="MS:1000521"]'
+  int_bit_type <- xml2::xml_attr(xml2::xml_find_first(xml_data, int_bit_xpath), "name")
+  int_precision <- sub(int_bit_type, pattern = "-bit float", replacement = "")
+  int_precision <- as.numeric(int_precision)/8
+
+  list(compression=compr, mz_precision=mz_precision, int_precision=int_precision)
+}
+
+#' Helper function to extract mzXML file metadata
 #'
-#' @examples
-#' mzXML_filename <- system.file("extdata", "190715_Poo_TruePooFK180310_Full2.mzXML", package = "RaMS")
-#' grabMzxmlData(mzXML_filename)
-grabMzxmlData <- function(filename){
-  xml_data <- xml2::read_xml(filename)
-
-  rt_xpath <- '//d1:scan'
-  rt_nodes <- xml2::xml_find_all(xml_data, rt_xpath)
-  rt_attrs <- xml2::xml_attr(rt_nodes, "retentionTime")
-  rt_vals <- as.numeric(gsub("PT|S", "", rt_attrs))
-
+#' @param xml_data mzXML data parsed by xml2
+#'
+#' @return A list of values used by other parsing functions, such as
+#' compression type and precision
+#'
+#' @export
+grabMzxmlMetadata <- function(xml_data){
   peak_metadata <- xml2::xml_find_first(xml_data, '//d1:peaks')
-
-  #SHOULD BE UPDATED WITH HELPER FUNCTIONS getMzPrecision etc.
   compr_type <- xml2::xml_attr(peak_metadata, "compressionType")
   compr <- switch(compr_type,
                   `zlib compression`="gzip",
@@ -170,42 +124,54 @@ grabMzxmlData <- function(filename){
   byte_order <- xml2::xml_attr(peak_metadata, "byteOrder")
   endi_enc <- switch(byte_order, `network`="big")
 
+  list(compression=compr, precision=precision, endi_enc=endi_enc)
+}
 
-  all_peak_nodes <- xml2::xml_text(xml2::xml_find_all(xml_data, '//d1:peaks'))
-  vals <- lapply(all_peak_nodes, function(binary){
+grabSpectraRt <- function(xml_nodes){
+  rt_xpath <- 'd1:scanList/d1:scan/d1:cvParam[@name="scan start time"]'
+  rt_nodes <- xml2::xml_find_all(xml_nodes, rt_xpath)
+  as.numeric(xml2::xml_attr(rt_nodes, "value"))
+}
+
+grabSpectraMz <- function(xml_nodes, file_metadata){
+  mz_xpath <- 'd1:binaryDataArrayList/d1:binaryDataArray[1]/d1:binary'
+  mz_vals <- xml2::xml_text(xml2::xml_find_all(xml_nodes, mz_xpath))
+  lapply(mz_vals, function(binary){
     decoded_binary <- base64enc::base64decode(binary)
     raw_binary <- as.raw(decoded_binary)
-    decomp_binary <- memDecompress(raw_binary, type = compr)
-    final_binary <- readBin(decomp_binary, what = "numeric",
-                            n=length(decomp_binary)/precision,
-                            size = precision, endian = endi_enc)
-    matrix(final_binary, ncol = 2, byrow = TRUE)
+    decomp_binary <- memDecompress(raw_binary, type = file_metadata$compression)
+    final_binary <- readBin(decomp_binary, what = "double",
+                            n=length(decomp_binary)/file_metadata$mz_precision,
+                            size = file_metadata$mz_precision)
   })
-  add_rts <- mapply(cbind, rt_vals, vals)
-  output <- as.data.frame(do.call(add_rts, what = "rbind")%*%diag(c(1/60, 1, 1)))
-  names(output) <- c("rt", "mz", "int")
-  output
 }
 
-getMzPrecision <- function(xml_data){
-  mz_precision_xpath <- '//d1:cvParam[@accession="MS:1000523"]'
-  mz_bit_type <- xml_attr(xml_find_first(xml_data, mz_precision_xpath), "name")
-  mz_precision <- sub(mz_bit_type, pattern = "-bit float", replacement = "")
-  mz_precision <- as.numeric(mz_precision)/8
+grabSpectraInt <- function(xml_nodes, file_metadata){
+  int_xpath <- 'd1:binaryDataArrayList/d1:binaryDataArray[2]/d1:binary'
+  int_vals <- xml2::xml_text(xml2::xml_find_all(xml_nodes, int_xpath))
+  int_vals <- lapply(int_vals, function(binary){
+    decoded_binary <- base64decode(binary)
+    raw_binary <- as.raw(decoded_binary)
+    decomp_binary <- memDecompress(raw_binary, type = file_metadata$compression)
+    final_binary <- readBin(decomp_binary, what = "double",
+                            n=length(decomp_binary)/file_metadata$int_precision,
+                            size = file_metadata$int_precision)
+  })
+
 }
 
-getCompression <- function(xml_data){
-  compr_xpath <- '//d1:cvParam[@accession="MS:1000574"]'
-  compr_type <- xml2::xml_attr(xml2::xml_find_first(xml_data, compr_xpath), "name")
-  compr <- switch(compr_type,
-                  `zlib compression`="gzip",
-                  `none`="none")
-}
+grabMzmlMS2 <- function(filename){
+  xml_data <- xml2::read_xml(filename)
 
-getIntPrecision <- function(xml_data){
-  int_bit_xpath <- '//d1:cvParam[@accession="MS:1000521"]'
-  int_bit_type <- xml2::xml_attr(xml2::xml_find_first(xml_data, int_bit_xpath), "name")
-  int_precision <- sub(int_bit_type, pattern = "-bit float", replacement = "")
-  int_precision <- as.numeric(int_precision)/8
+  file_metadata <- grabMzmlMetadata(xml_data)
+  msn_xpath <- '//d1:cvParam[@name="MSn spectrum"]/parent::d1:spectrum'
+  msn_nodes <- xml2::xml_find_all(xml_data, msn_xpath)
+
+  rt_vals <- grabSpectraRt(msn_nodes)
+  mz_vals <- grabSpectraMz(msn_nodes, file_metadata)
+  int_vals <- grabSpectraInt(msn_nodes, file_metadata)
+
+  data.frame(rt=rep(rt_vals, sapply(mz_vals, length)),
+             mz=unlist(mz_vals), int=unlist(int_vals))
 }
 
