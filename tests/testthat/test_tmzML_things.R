338716e62bf03e588fd4d0548911e26da3eec6f5
wkumler <wkumler@uw.edu>
2022-02-14 11:04:51 -0800

    Adding unit tests

--- tests/testthat/test_tmzML_things.R
+++ tests/testthat/test_tmzML_things.R
@@ -19,9 +19,20 @@ test_that("tmzML conversion works for mzXMLs", {
     verbosity = 0), "temp_tmzMLs/DDApos_2.tmzML")
 })
 
+test_that("tmzML warns if filename doesn't end in .tmzML", {
+  expect_warning(RaMS:::tmzmlMaker(
+    input_filename = mzML_filenames[1],
+    output_filename = output_filename,
+    verbosity = 0))
+})
 
 
 
 # Reading tmzMLs ----
+msdata <- grabMSdata(tmzml_filename)
+test_that("tmzML can be read", {
+  expect_s3_class(msdata, "msdata_connection")
+  expect_type(msdata, "list")
+})
 
 unlink(output_folder, recursive = TRUE, force = TRUE)
