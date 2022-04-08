937a852afc12b18f930c28b9f2749dd8aa827909
wkumler <wkumler@uw.edu>
2022-04-08 16:55:22 -0700

    Adding vignettes to GitHub via doc

---
+++ doc/Intro-to-tmzML.R
@@ -0,0 +1,58 @@
+## ----setup, include = FALSE---------------------------------------------------
+options(rmarkdown.html_vignette.check_title = FALSE)
+
+## ----listfiles----------------------------------------------------------------
+files_to_convert <- list.files(
+  system.file("extdata", package = "RaMS"), full.names = TRUE, pattern = "mzML"
+)[2:4]
+
+## ----maketmzmls---------------------------------------------------------------
+library(RaMS)
+# Create a folder to hold the new files
+dir.create("tmzMLs")
+
+# Convert a single file
+file_to_create <- "tmzMLs/LB12HL_AB.tmzML"
+tmzmlMaker(input_filename = files_to_convert[1], output_filename = file_to_create)
+
+# Convert multiple files
+files_to_create <- paste0("tmzMLs/", basename(files_to_convert))
+# Make sure they end in .tmzML!
+files_to_create <- gsub(x = files_to_create, "\\.mzML.*", ".tmzML")
+# Loop over each file input/output pair
+created_files <- mapply(tmzmlMaker, files_to_convert, files_to_create)
+
+## ----grabmsdata---------------------------------------------------------------
+msdata <- grabMSdata(created_files)
+
+## ----subset$------------------------------------------------------------------
+ms_data_table <- msdata$MS1[mz%between%pmppm(152.05723, 5)]
+
+## ----ggplot-------------------------------------------------------------------
+library(ggplot2)
+ggplot(ms_data_table) + geom_line(aes(x=rt, y=int, color=filename)) + xlim(8, 9.5)
+
+## ----dplyr--------------------------------------------------------------------
+library(dplyr)
+ms_data_table %>%
+  filter(rt%between%c(8.4, 8.9)) %>%
+  group_by(filename) %>%
+  summarise(area=sum(int))
+
+## ----print msdata_connection--------------------------------------------------
+print(msdata)
+
+## ----str msdata_connection----------------------------------------------------
+str(msdata)
+
+## ----expected errors, error=TRUE----------------------------------------------
+# Cannot order data by mass
+msdata$MS1[-mz]
+# Cannot request the top few rows
+msdata[1:10,]
+# Cannot subset by retention time
+msdata$MS1[rt %between% c(8, 9.5)]
+
+## ----cleanup------------------------------------------------------------------
+unlink("tmzMLs", recursive = TRUE)
+
---
+++ doc/Minifying-files-with-RaMS.R
@@ -0,0 +1,117 @@
+## ----setup, include = FALSE---------------------------------------------------
+options(rmarkdown.html_vignette.check_title = FALSE)
+options(tidyverse.quiet = TRUE)
+
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  out.width = "80%",
+  fig.align = 'center',
+  fig.height = 3,
+  fig.width = 6.5
+)
+
+## ----minifyfile, results='hide'-----------------------------------------------
+library(RaMS)
+msdata_files <- list.files(
+  system.file("extdata", package = "RaMS"), full.names = TRUE, pattern = "mzML"
+)
+
+initial_filename <- msdata_files[1]
+output_filename <- gsub(x=paste0("minified_", basename(initial_filename)), "\\.gz", "")
+
+masses_of_interest <- c(118.0865, 138.0555)
+minifyMSdata(files = initial_filename, output_files = output_filename, 
+             mz_include = masses_of_interest, ppm = 10, warn = FALSE)
+
+## ----opendata, results='hide'-------------------------------------------------
+init_msdata <- grabMSdata(initial_filename)
+msdata <- grabMSdata(output_filename)
+
+## ----showmini-----------------------------------------------------------------
+knitr::kable(head(msdata$MS1, 3))
+knitr::kable(head(msdata$MS2, 3))
+
+## ----TICBPCdiffs--------------------------------------------------------------
+par(mfrow=c(2, 1), mar=c(2.1, 2.1, 1.1, 0.1))
+plot(init_msdata$BPC$rt, init_msdata$BPC$int, type = "l", main = "Initial BPC")
+plot(msdata$BPC$rt, msdata$BPC$int, type = "l", main = "New BPC")
+
+## ----cleanmini, include=FALSE-------------------------------------------------
+layout(1)
+unlink(output_filename)
+
+## ----vectorizedmini-----------------------------------------------------------
+dir.create("mini_mzMLs/")
+output_files <- paste0("mini_mzMLs/", basename(msdata_files))
+output_files <- gsub(x=output_files, "\\.gz", "")
+
+minifyMSdata(files = msdata_files, output_files = output_files, verbosity = 0,
+             mz_include = masses_of_interest, ppm = 10, warn = FALSE)
+
+## ----ggplotmini, fig.height=2-------------------------------------------------
+mini_msdata <- grabMSdata(output_files, verbosity = 0)
+
+library(ggplot2)
+ggplot(mini_msdata$BPC) + geom_line(aes(x=rt, y=int, color=filename)) + theme_bw()
+
+## ----cleanminimulti, include=FALSE--------------------------------------------
+unlink("mini_mzMLs", recursive = TRUE)
+
+## ----mzs to include, eval=FALSE-----------------------------------------------
+#  raw_stans <- read.csv(paste0("https://raw.githubusercontent.com/",
+#                               "IngallsLabUW/Ingalls_Standards/",
+#                               "b098927ea0089b6e7a31e1758e7c7eaad5408535/",
+#                               "Ingalls_Lab_Standards_NEW.csv"))
+#  
+#  mzs_to_include <- as.numeric(unique(raw_stans[raw_stans$Fraction1=="HILICPos",]$m.z))
+#  # Include glycine betaine isotopes for README demo
+#  mzs_to_include <- c(mzs_to_include, 119.0899, 119.0835)
+
+## ----download the mzXMLs, eval=FALSE------------------------------------------
+#  if(!dir.exists("vignettes/data"))dir.create("vignettes/data")
+#  base_url <- "ftp://ftp.ebi.ac.uk/pub/databases/metabolights/studies/public/MTBLS703/"
+#  chosen_files <- paste0(base_url, "161024_Smp_LB12HL_", c("AB", "CD", "EF"), "_pos.mzXML")
+#  new_names <- gsub(x=basename(chosen_files), "161024_Smp_", "")
+#  
+#  mapply(download.file, chosen_files, paste0("vignettes/data/", new_names),
+#         mode = "wb", method = "libcurl")
+
+## ----get DDA, eval=FALSE------------------------------------------------------
+#  file.copy(from = paste0("Z:/1_QEdata/2016/2016_Katherine_1335_LightB12_",
+#                          "Experiment/170223_KRH_Rerun_1335_LightB12_Exp_HILIC/",
+#                          "positive/",
+#                          "170223_Poo_AllCyanoAqExtracts_DDApos_2.mzXML"),
+#            to = "vignettes/data/DDApos_2.mzXML", overwrite = TRUE)
+
+## ----minify, warning=FALSE, eval=FALSE----------------------------------------
+#  library(RaMS)
+#  
+#  if(!dir.exists("inst/extdata"))dir.create("inst/extdata")
+#  init_files <- list.files("vignettes/data/", full.names = TRUE)
+#  out_files <- paste0("inst/extdata/", basename(init_files))
+#  minifyMSdata(files = init_files, output_files = out_files, warn = FALSE,
+#               mz_include = mzs_to_include, ppm = 20)
+
+## ----msconvertchunk, eval=FALSE-----------------------------------------------
+#  system("msconvert inst/extdata/*.mzXML -o inst/extdata/temp --noindex")
+#  system("msconvert --mzXML inst/extdata/*.mzXML -o inst/extdata/temp --noindex")
+#  system('msconvert inst/extdata/temp/*.mzML --filter \"scanTime [240,900]\" -o inst/extdata -g')
+#  system('msconvert inst/extdata/temp/*.mzXML --mzXML --filter \"scanTime [240,900]\" -o inst/extdata -g')
+
+## ----renameremove, eval=FALSE-------------------------------------------------
+#  init_files <- list.files("inst/extdata", full.names = TRUE)
+#  new_names <- paste0("inst/extdata/", gsub(x=init_files, ".*(Smp_|Extracts_)", ""))
+#  file.rename(init_files, new_names)
+#  
+#  unlink("inst/extdata/temp", recursive = TRUE)
+#  file.remove(list.files("inst/extdata", pattern = "mzXML$", full.names = TRUE))
+#  file.remove(paste0("inst/extdata/", c("LB12HL_CD.mzXML.gz", "LB12HL_EF.mzXML.gz")))
+
+## ----checkquality, eval=FALSE-------------------------------------------------
+#  MSnbase::readMSData(list.files("inst/extdata", full.names = TRUE)[1], msLevel. = 1)
+#  RaMS::grabMSdata(new_names[1])
+
+## ----vigdata cleanup, eval=FALSE----------------------------------------------
+#  unlink("vignettes/data", recursive = TRUE)
+
