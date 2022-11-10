7e6792e81efb76f9bd1633ef8c8157c383607e9f
wkumler <wkumler@uw.edu>
2022-11-10 11:46:50 -0800

    Adding more tests for chroms to improve coverage

--- tests/testthat/test_chroms.R
+++ tests/testthat/test_chroms.R
@@ -1,9 +1,20 @@
 
 msdata_MRM_chroms <- grabMSdata(files = list.files(system.file("extdata", package = "RaMS"),
-                              pattern = "wk_chrom.mzML.gz", full.names = TRUE))
+                              pattern = "wk_chrom.mzML.gz", full.names = TRUE),
+                              grab_what="chroms")
 
 test_that("chroms are grabbed when using chroms", {
-  expect_identical(mzML_everything, mzML_data)
+  expect_identical(names(msdata_MRM_chroms), "chroms")
+  expect_identical(names(msdata_MRM_chroms$chroms),
+                   c("chrom_type", "chrom_index", "target_mz", "product_mz",
+                     "rt", "int", "filename"))
+})
+
+msdata_MRM_everything <- grabMSdata(files = list.files(system.file("extdata", package = "RaMS"),
+                                                       pattern = "wk_chrom.mzML.gz", full.names = TRUE),
+                                    grab_what=c("everything", "chroms"))
+test_that("everything works with chroms", {
+  expect_identical(names(msdata_MRM_everything), c("MS1", "MS2", "BPC", "TIC", "chroms", "metadata"))
 })
 
 test_that("warning thrown when requesting chroms from mzXML", {
